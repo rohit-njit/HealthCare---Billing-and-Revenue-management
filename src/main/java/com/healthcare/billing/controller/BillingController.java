e2e21f8669db199dc3c77b00096c7fd0c95bbe7f
chandresh sikarwar <cs62@njit.edu>
2021-11-03 15:17:31 -0400

    API interface

--- src/main/java/com/healthcare/billing/controller/BillingController.java
+++ src/main/java/com/healthcare/billing/controller/BillingController.java
@@ -1,25 +1,42 @@
 package com.healthcare.billing.controller;
 
-import com.healthcare.billing.controller.model.ProcedureRate;
-import com.healthcare.billing.service.BillingService;
-import com.healthcare.billing.service.BillingServiceImpl;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class BillingController {
 
     @RequestMapping("/procedures/{procedureId}/rates")
-    public ProcedureRate getRate(@PathVariable String procedureId) {
-        ProcedureRate pr = new ProcedureRate();
-        pr.setId(procedureId);
-        pr.setRate(100);
-        pr.setProcedureId("Random-id");
-        BillingService service = new BillingServiceImpl();
-        service.getProcedureRate(procedureId);
-        return pr;
+    public String getRate(@PathVariable String procedureId) {
+        return "{\"message\" : \"This API is under construction.\"}";
     }
+
+    @RequestMapping("/procedures")
+    public String getProcedures() {
+        return "{\"message\" : \"This API is under construction.\"}";
+    }
+
+    @RequestMapping("/claims")
+    public String getClaims() {
+        return "{\"message\" : \"This API is under construction.\"}";
+    }
+
+    @RequestMapping("/claims/{claimId}")
+    public String getClaim(@PathVariable String claimId) {
+        return "{\"message\" : \"This API is under construction.\"}";
+    }
+
+    @RequestMapping(value = "/claims/{claimId}", method = RequestMethod.POST)
+    public String createClaim(@PathVariable String claimId) {
+        return "{\"message\" : \"This API is under construction.\"}";
+    }
+    @RequestMapping(value = "/claims/{claimId}", method = RequestMethod.PUT)
+    public String updateClaim(@PathVariable String claimId) {
+        return "{\"message\" : \"This API is under construction.\"}";
+    }
+    @RequestMapping(value = "/claims/{claimId}", method = RequestMethod.DELETE)
+    public String deleteClaim(@PathVariable String claimId) {
+        return "{\"message\" : \"This API is under construction.\"}";
+    }
+
 
 }
